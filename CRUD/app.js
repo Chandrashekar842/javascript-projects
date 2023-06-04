55ceee8f29717f30ef0615988d433d8c99a6899d
Chandu <m.chandrashekar021@gmail.com>
2023-06-04 12:36:41 +0530

    comments added

--- CRUD/app.js
+++ CRUD/app.js
@@ -142,3 +142,27 @@ function updateData(index) {
         }
     }
 }
+const todo = (callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange',() => {
+    if(request.readyState === 4 && request.status === 200) {
+        callback(undefined, request.responseText);
+    } 
+    else if(request.readyState === 4)
+    {
+        callback('could not fetch data', undefined);
+    }
+    });
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+}
+
+todo((err, data) => {
+    console.log("callback is fired");
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+});
